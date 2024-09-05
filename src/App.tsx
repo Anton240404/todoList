f4203e9f786e29f1646ae65064922e38f7beff98
Anton24044 <anton.agafonov.97@bk.ru>
2024-09-05 19:21:53 +0300

    123

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
-
+import {useState} from "react";
 
 function App() {
 
+    const [add, setAdd] = useState('')
 
-  return (
-        <p>Hello World!</p>
-  )
+
+
+    return (
+        <div>
+        <input type="text" onChange={e => setAdd(e.target.value)}/>
+        <button onClick={}>click</button>
+        </div>
+    )
 }
 
 export default App
