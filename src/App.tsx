64a6f2a68f3bbe2fbdbc44d21cb75cf9a3465b37
artur <example@email.com>
2024-09-17 21:14:27 +0500

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,29 @@
 import { useState } from "react"
-import "./componets/testArray"
-import "./componets/generics"
 
 function App() {
-    const [text, setText] = useState("")
-    const [todos, setTodos] = useState([])
+  const [text, setText] = useState("")
+  const [todos, setTodos] = useState([])
 
-    const handleClickText = () => {
-        const copyTodos = [...todos]
-        copyTodos.push(text)
-        setTodos(copyTodos)
-        setText("")
-        /* todos.push(text)
+  const handleClickText = () => {
+    const copyTodos = [...todos]
+    copyTodos.push(text)
+    setTodos(copyTodos)
+    setText("")
+    /* todos.push(text)
         setText("");*/
-    }
+  }
 
-    return (
-        <div>
-            <input type="text" onChange={(e) => setText(e.target.value)} value={text} />
-            <button onClick={() => handleClickText()}>click</button>
-            <div>{todos.join(";")}</div>
-        </div>
-    )
+  return (
+    <div>
+      <input
+        type="text"
+        onChange={(e) => setText(e.target.value)}
+        value={text}
+      />
+      <button onClick={() => handleClickText()}>click</button>
+      <div>{todos.join(";")}</div>
+    </div>
+  )
 }
 
 export default App
