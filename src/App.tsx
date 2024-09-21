1b7d29a376d225f9ccf51aa29f9b1c663f00c301
artur <example@email.com>
2024-09-21 17:38:21 +0500

    key

--- src/App.tsx
+++ src/App.tsx
@@ -16,16 +16,32 @@ function App() {
         copyTodos.splice(index, 1);
         setText(copyTodos);
     };
-
     return (
         <div>
-            <button onClick={handleClickDelete}></button>
+            <ul>
+                {nums.map((x, i) => (
+                    <li key={i}>
+                        <input type="text" />
+                        text = {x.text}{' '}
+                        <button
+                            onClick={() => {
+                                const copy = [...nums];
+                                copy.splice(i, 1);
+                                setNums(copy);
+                            }}
+                        >
+                            Delete
+                        </button>
+                    </li>
+                ))}
+            </ul>
+            {/* <button onClick={handleClickDelete}></button>
             <input
                 type="text"
                 onChange={(e) => setText(e.target.value)}
                 value={text}
             />
-            <button onClick={() => handleClickText()}>click</button>
+            <button onClick={() => handleClickText()}>click</button> */}
         </div>
     );
 }
