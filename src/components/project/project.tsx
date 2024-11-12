1c7b5226a93eb1245fe84b840c8c292d128747df
Anton24044 <anton.agafonov.97@bk.ru>
2024-11-12 19:19:31 +0200

    refactor code Project

--- src/components/project/project.tsx
+++ src/components/project/project.tsx
@@ -1,8 +1,8 @@
 import './style.css';
 import avatar from './assets/avatar.png';
-import foto1 from './assets/foto1.jpg';
-import foto2 from './assets/foto2.png';
-import vector from './assets/vector.png';
+import photo1 from './assets/photo1.jpg';
+import photo2 from './assets/photo2.png';
+import vector from './assets/vector.svg';
 import avatar2 from './assets/avatar2.jpg';
 import avatar3 from './assets/avatar3.png';
 import cardImage from './assets/card-image.png';
@@ -15,16 +15,16 @@ export function Project() {
                 <img className="avatar" src={avatar} alt="avatar" />
                 <div className="text">
                     <h1 className="leslie">Leslie Alexander</h1>
-                    <h2 className="Johnson">Johnson & Johnson</h2>
+                    <h2 className="johnson">Johnson & Johnson</h2>
                 </div>
             </div>
             <div className="information">
-                <img className="foto1" src={foto1} alt="foto" />
-                <img src={foto2} alt="foto2" />
+                <img src={photo1} />
+                <img src={photo2} />
             </div>
-            <div className="bodyText">
-                <h1 className="WebText">Web Design templates Selection</h1>
-                <p className="textInfo">
+            <div className="body-text">
+                <h1 className="web-text">Web Design templates Selection</h1>
+                <p className="text-info">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                     do eiusmod tempor incididunt ut labore et dolore magna
                     aliqua.
@@ -39,37 +39,64 @@ export function Project() {
                 placeholder="Write a comment"
                 rows={4}
             ></textarea>
-            <div className="footer-commend">
-                <img className="avatar2" src={avatar2} alt={avatar} />
-                <div className="Name-minute">
-                    <p className="mitchell">Mitchell</p>
-                    <p className="minutes">25 minutes ago</p>
+
+            <div className="footer-comment">
+                <div className="box1">
+                    <div>
+                        <img className="avatar2" src={avatar2} alt={avatar} />
+                    </div>
+                    <div className="container-small">
+                        <div className="name-time">
+                            <div className="mitchell">Mitchell</div>
+                            <div className="times">25 minutes ago</div>
+                        </div>
+                        <p className="text1">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit sed do eiusmod.
+                        </p>
+                    </div>
                 </div>
-                <text className="commend1">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit sed
-                    do eiusmod.
-                </text>
-                <div className="heart-reply">
-                    <img className="heart" src={vector} alt="Vector" />
-                    <p className="number34">18</p>
+                <div className="action">
+                    <div className="like-action">
+                        <img className="heart" src={vector} alt="Vector" />
+                        <p className="number34">18</p>
+                    </div>
                     <p className="reply">Reply</p>
                 </div>
             </div>
-            <div className="footer-commend2">
-                <img src={avatar3} alt={avatar} />
-                <div className="Name-minute2">
-                    <p className="Robert-Fox">Robert Fox</p>
-                    <p className="minutes2">3 minutes ago</p>
+
+            <div className="footer-comment2">
+                <div className="box2">
+                    <div>
+                        <img
+                            className="avatar-comment"
+                            src={avatar3}
+                            alt={avatar}
+                        />
+                    </div>
+                    <div className="container-small2">
+                        <div className="name-times2">
+                            <p className="robert-Fox">Robert Fox</p>
+                            <p className="times2">3 minutes ago</p>
+                        </div>
+                        <div className="photo-comment">
+                            <p>
+                                Dolor sit ameteiusmod consectetur <br />
+                                adipiscing elit.
+                            </p>
+                            <img
+                                className="card-image"
+                                src={cardImage}
+                                alt={cardImage}
+                            />
+                        </div>
+                    </div>
                 </div>
-                <div className="foto-commend">
-                    <text>
-                        Dolor sit ameteiusmod consectetur adipiscing elit.
-                    </text>
-                    <img src={cardImage} alt={cardImage} />
-                </div>
-                <div className="heart-reply2">
-                    <img className="heart" src={vector} alt="Vector" />
-                    <p className="number34">2</p>
+                <div className="action">
+                    <div className="like-action">
+                        <img src={vector} alt="Vector" />
+                        <p className="number34">2</p>
+                    </div>
                     <p className="reply">Reply</p>
                 </div>
             </div>
