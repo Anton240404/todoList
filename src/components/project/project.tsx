89f84a181f5bbfa3eee151afa5071614c5153979
Anton24044 <anton.agafonov.97@bk.ru>
2024-11-15 14:20:17 +0200

    debag HW

--- src/components/project/project.tsx
+++ src/components/project/project.tsx
@@ -1,10 +1,10 @@
 import './style.css';
-import avatar from './assets/avatar.png';
-import photo1 from './assets/photo1.jpg';
-import photo2 from './assets/photo2.png';
+import avatar from './assets/avatar.jpg';
+import photo4k from './assets/photo4k.jpg';
+import photo4k2 from './assets/photo4k2.jpg';
 import vector from './assets/vector.svg';
 import avatar2 from './assets/avatar2.jpg';
-import avatar3 from './assets/avatar3.png';
+import avatar3 from './assets/avatar3.jpg';
 import cardImage from './assets/card-image.png';
 import avatar4 from './assets/avatar4.png';
 
@@ -14,13 +14,13 @@ export function Project() {
             <div className="header">
                 <img className="avatar" src={avatar} alt="avatar" />
                 <div className="text">
-                    <h1 className="leslie">Leslie Alexander</h1>
-                    <h2 className="johnson">Johnson & Johnson</h2>
+                    <h1 className="author-header">Leslie Alexander</h1>
+                    <h2 className="company">Johnson & Johnson</h2>
                 </div>
             </div>
             <div className="information">
-                <img src={photo1} />
-                <img src={photo2} />
+                <img src={photo4k} />
+                <img src={photo4k2} />
             </div>
             <div className="body-text">
                 <h1 className="web-text">Web Design templates Selection</h1>
@@ -32,7 +32,7 @@ export function Project() {
             </div>
             <div className="vector">
                 <img className="heart" src={vector} alt="Vector" />
-                <p className="number34">34</p>
+                <p className="number">34</p>
             </div>
             <textarea
                 className="comment-box"
@@ -47,21 +47,21 @@ export function Project() {
                     </div>
                     <div className="container-small">
                         <div className="name-time">
-                            <div className="mitchell">Mitchell</div>
+                            <div className="author-body">Mitchell</div>
                             <div className="times">25 minutes ago</div>
                         </div>
                         <p className="text1">
                             Lorem ipsum dolor sit amet, consectetur adipiscing
                             elit sed do eiusmod.
                         </p>
+                        <div className="action">
+                            <div className="like-action">
+                                <img className="heart" src={vector} />
+                                <p className="number">18</p>
+                                <p className="reply">Reply</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div className="action">
-                    <div className="like-action">
-                        <img className="heart" src={vector} alt="Vector" />
-                        <p className="number34">18</p>
-                    </div>
-                    <p className="reply">Reply</p>
                 </div>
             </div>
 
@@ -76,7 +76,7 @@ export function Project() {
                     </div>
                     <div className="container-small2">
                         <div className="name-times2">
-                            <p className="robert-Fox">Robert Fox</p>
+                            <p className="author-body2">Robert Fox</p>
                             <p className="times2">3 minutes ago</p>
                         </div>
                         <div className="photo-comment">
@@ -89,15 +89,15 @@ export function Project() {
                                 src={cardImage}
                                 alt={cardImage}
                             />
+                            <div className="action">
+                                <div className="like-action">
+                                    <img src={vector} alt="Vector" />
+                                    <p className="number">2</p>
+                                    <p className="reply">Reply</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="action">
-                    <div className="like-action">
-                        <img src={vector} alt="Vector" />
-                        <p className="number34">2</p>
-                    </div>
-                    <p className="reply">Reply</p>
                 </div>
             </div>
             <div className="footer-write">
