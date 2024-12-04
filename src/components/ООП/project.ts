acd29715fc3581d7450fc498803f26b3cc36c60f
artur <example@a.b>
2024-12-04 14:56:48 +0500

    interface-vs-abstract

--- src/components/ООП/project.ts
+++ src/components/ООП/project.ts
@@ -9,6 +9,10 @@ abstract class Transport {
         this.capacity = capacity;
     }
 
+    public printName(): void {
+        console.log(this.name);
+    }
+
     public abstract move(): void;
 
     public getName(): string {
