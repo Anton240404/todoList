81db9f2d25a861a6702d488990156bbce2d9ad7f
Anton24044 <anton.agafonov.97@bk.ru>
2024-12-23 20:46:06 +0300

    map v2

--- src/components/lists/numbers-list.tsx
+++ src/components/lists/numbers-list.tsx
@@ -1,17 +1,25 @@
 const numbers = [1, 2, 3];
-const numbers2 = [<li>1</li>, 2, 3];
+const strings = ['1', '2', '3'];
 
 import React from 'react';
 
 export function TableNumbers() {
-    const newArray: string[] = [];
-
-    numbers.forEach((elem) => {
-        const res = elem + '';
-        newArray.push(res);
+    // Метод map нужен для трансформации каждого элемента массива. Например, если из массива [1, 2, 3] нужно сделать ['1', '2', '3']
+    // Название метода map можно мысленно заменить на слово "transform"
+    // В этот метод нужно отправить callback, в который придет один элемент массива, а мы должны ВЕРНУТЬ во что мы трансформируем
+    // Метод map из всех этих вернувшихся трасформированных значений соберет новый массив
+    // const fn = (elem) => elem + ''
+    // fn(1) -> "1"
+    // fn(2) -> "2"
+    // fn(3) -> "3"
+    // const result = [fn(1), fn(2), fn(3)] // ['1', '2', '3']
+    // return result
+    const res = numbers.map((elem) => {
+        const x1 = elem + '';
+        return x1;
     });
 
-    console.log(newArray);
+    console.log(res);
 
     return (
         <div>
