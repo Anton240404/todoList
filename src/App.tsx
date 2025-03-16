ebf9c90498a81caaefad25669d9b80687c683098
artur <example@a.b>
2025-03-16 20:59:32 +0500

    1

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import { PaginationTest } from './components/pagination';
 import { StepsTest } from './components/steps';
 import { CollapseTest } from './components/collapse';
 import { PasswordReq } from './components/password-requirements/password-requirements';
-import { Test } from './components/test';
 import { MyInput } from './components/my-input/my-input';
 import { Accordion, Spoiler } from './components/spoiler';
 import { Breadcrumbs } from './Breadcrumbs/breadcrumbs';
 import { AlertComponent } from './Alert-component/AlertComponent';
+import { AvatarGroup } from './components/avatar-group/avatar-group';
 
 // 1) Добавить лейбл label="Выберите тип обучения" - подпись к селекту
 // 2) Выделять уже выбранный айтем
@@ -44,9 +44,17 @@ export const App = () => {
             />*/}
             {/*   <Accordion />*/}
             {/* <Breadcrumbs items={breadcrumbs} />*/}
-            <AlertComponent
+            {/* <AlertComponent
                 severity="success"
                 text="This is a success Alert."
+            /> */}
+            <AvatarGroup
+                items={[
+                    { url: 'https//:example.com', firstName: '', lastName: '' },
+                    { url: '', firstName: 'Иван', lastName: 'Иванов' },
+                    { url: '', firstName: 'Иван', lastName: 'Иванов' },
+                ]}
+                visibleCount={6}
             />
         </div>
     );
---
+++ src/components/avatar-group/avatar-group.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import style from './avatar.module.css';
+
+type Props = {
+    items: Item[];
+};
+
+type Item = {
+    url: string;
+    firstName: string; // имя
+    lastName: string; // фамилия
+};
+
+// Стейт нужен, если данные будут меняться, если нам необходимо,
+// что то отрисовать без изменений данных, стейт не нужен.
+
+const x = 'test';
+// x.slice(0, 1)
+// x[0]
+
+export function AvatarGroup(props: Props) {
+    return (
+        <div className={style.root}>
+            {props.items.map((item) => (
+                <div className={style.avatar}>
+                    {item.url ? item.url : item.firstName}
+                </div>
+            ))}
+        </div>
+    );
+}
+
+// 1) Отрисовать все круги
+// 1.1) Если firstName и lastName иначе url
+// 2) Отрисовать первые visibleCount кругов
+// 3) Узнать, сколько не отрисовали (+6)
+// 4) Нарисовать +6 (если неотрисованных > 0)
+
+// <AvatarGroup
+// items={[
+//  {url: 'https://example.com', firstName: '', lastName: '' }
+//  {url: '', firstName: 'Иван', lastName: 'Иванов' }
+// ]}
+//  visibleCount={6}
+//  />
