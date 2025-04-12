9cb41096d3819edcc91287c729357770da188028
Anton24044 <anton.agafonov.97@bk.ru>
2025-04-12 12:39:59 +0300

    исправил роуты и удалил компонент Header()

--- src/components/training/react-dom/router-dom.tsx
+++ src/components/training/react-dom/router-dom.tsx
@@ -17,39 +17,16 @@ export function RouterDom() {
         <div>
             {/*1*/}
             <nav>
-                <Link to="/">Главная</Link>
-                <a href="/">Главная 2</a>
+                <Link to={nav.base}>Главная</Link>
                 <Link to={nav.about}>О нас</Link>
                 <Link to={nav.user(42)}>Пользователь 42</Link>
             </nav>
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path={nav.base} element={<Home />} />
                 <Route path={nav.about} element={<About />} />
                 {/* 3 */}
                 <Route path={nav.user(':id')} element={<User />} />
             </Routes>
-        </div>
-    );
-}
-
-function Header() {
-    const location = useLocation();
-
-    const [searchParams, setSearchParams] = useSearchParams();
-
-    return (
-        <div>
-            Header {location.pathname}
-            <p>{searchParams.get('sort')}</p>
-            {location.pathname !== '/a' && <button>Вернуться на А</button>}
-            {location.pathname !== '/b' && <button>Вернуться на B</button>}
-            {location.pathname !== '/c' && <button>Вернуться на C</button>}
-            <button onClick={() => setSearchParams({ sort: 'asc' })}>
-                Sort ascending
-            </button>
-            <button onClick={() => setSearchParams({ sort: 'desc' })}>
-                Sort descending
-            </button>
         </div>
     );
 }
